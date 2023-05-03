0b64e1f5a6e04a37c9d11250fd450decdb42ab4f
Anshu Sinha <anshujuly2@gmail.com>
2023-05-03 12:24:07 +0530

    Add a code to able to view movie list items

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# ------------------------- MODULES ------------------------- #
 # imported Flask, render_template, redirect, url_for and request class from flask module
 from flask import Flask, render_template, redirect, url_for, request
 
@@ -19,19 +20,70 @@ from wtforms.validators import DataRequired
 # imported requests module
 import requests
 
+
+# ------------------------- APP CONFIGURATION ------------------------- #
 # created instance of Flask class
 app = Flask(__name__)
 
-# created instance of SQLAlchemy class
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# created instance of Bootstrap class
 Bootstrap(app)
 
+# created instance of SQLAlchemy class
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# ------------------------- TABLE ------------------------- #
+# created Movie table
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250))
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250))
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+# with app.app_context():
+#     db.create_all()
+
+#     def add_movie():
+#         new_movie = Movie(title='Phone Booth',
+#                            year=2002,
+#                            description='Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist\'s sniper rifle. Unable to leave or receive outside help, Stuart\'s negotiation with the caller leads to a jaw-dropping climax.',
+#                            rating=7.3,
+#                            ranking=10,
+#                            review='One of the best movies I\'ve ever seen!',
+#                            img_url='https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg'
+#                            )
+
+#         db.session.add(new_movie)
+#         db.session.commit()
+
+#     add_movie()
+
+
+# ------------------------- ROUTES ------------------------- #
 # route decorator to tell Flask what URL should trigger the function
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = Movie.query.order_by(Movie.rating).all()
+    return render_template("index.html", movies=all_movies)
 
 
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == 'POST':
+        pass
+
+
+# ------------------------- MAIN ------------------------- #
 # checks if name is equal to main and runs the app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
